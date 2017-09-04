6accf9834674eb131c5320f1b38f5230dc97631a
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-09-04 23:20:05 +0800

    first commit

---
+++ sumOfAllNumberInRange.js
@@ -0,0 +1,27 @@
+
+
+function sumAll(arr) {
+
+	var newArray = [];
+	var bestArray;
+
+	var high = newArray.push(Math.max(arr[0],arr[1]));
+	var low = newArray.push(Math.min(arr[0],arr[1]));
+
+	for (i = newArray[0]; i <= newArray[1]; i++) {
+		bestArray.push(i);
+	}
+
+	/*var sumNum = newArray.reduce(function(sum,order) {
+		
+	});*/
+
+	return bestArray;
+
+}
+
+console.log(sumAll([1, 4]));//10
+console.log(sumAll([4, 1]));//10
+//sumAll([5, 10]);//45
+//sumAll([10, 5]);//45
+
